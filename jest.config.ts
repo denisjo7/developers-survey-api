1ba59f0af6fbca0d4111a08bbf92ffbfb826c016
Denis Jonathan <denisjonathan7@gmail.com>
2022-04-19 19:38:18 -0300

    chore: ignore protocols index and sign up protocols in jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,10 @@ export default {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/presentation/protocols/index.ts',
+    '<rootDir>/src/presentation/controllers/signup/signup-protocols.ts'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   transform: {
